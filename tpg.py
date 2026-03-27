7e760429f8d02c47bc515f07ff8aeefec2a1bd37
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2026-03-26 22:41:43 -0500

    Move TPG code

--- tpg.py
+++ tpg.py
@@ -1,5 +1,7 @@
 from accounts.stripe.tasks import send_welcome_email_job
 
+from rangefilter.filters import DateRangeFilter
+
 from www.shop.config import (
   NEWSLETTER_NICKNAME, FORECAST_ALL_NICKNAME, FORECAST_MAJ_NICKNAME,
 )
@@ -78,3 +80,93 @@ class View:
       contact.lists.remove(contact_list)
 
 
+class BalanceTransactionResource(resources.ModelResource):
+  """Import-Export Resource for BalanceTransactions."""
+  customer_name = Field()
+  customer_email = Field()
+  invoice_url = Field()
+  products = Field()
+
+  class Meta:
+    model = BalanceTransaction
+    fields = (
+      'id',
+      'customer_name',
+      'customer_email',
+      'products',
+      'type',
+      'currency',
+      'amount',
+      'fee',
+      'net',
+      'status',
+      'available_on',
+      'invoice_url',
+    )
+    export_order = fields
+
+  def dehydrate_customer_name(self, bt: BalanceTransaction) -> str:
+    if charge := bt.charges.first():
+      name = charge.customer.name
+    elif refund := bt.refunds.first():
+      name = refund.charge.customer.name
+    else:
+      name = 'N/A'
+    return name
+
+  def dehydrate_customer_email(self, bt: BalanceTransaction) -> str:
+    if charge := bt.charges.first():
+      email = charge.customer.email
+    elif refund := bt.refunds.first():
+      email = refund.charge.customer.email
+    else:
+      email = 'N/A'
+    return email
+
+  def dehydrate_invoice_url(self, bt: BalanceTransaction) -> str:
+    if charge := bt.charges.first():
+      url = charge.invoice.invoice_pdf
+    else:
+      url = 'N/A'
+    return url
+
+  def dehydrate_products(self, bt: BalanceTransaction) -> str:
+    if charge := bt.charges.first():
+      items = charge.invoice.subscription.items.all()
+      products = ', '.join(items.values_list('price__nickname', flat=True))
+    else:
+      products = 'N/A'
+    return products
+
+
+@admin.register(BalanceTransaction)
+class BalanceTransactionAdmin(
+  ExportActionMixin,
+  StripeModelAdmin[BalanceTransaction]
+):
+  resource_classes = (
+    BalanceTransactionResource,
+  )
+  formats = (
+    base_formats.CSV,
+    base_formats.XLS,
+    base_formats.XLSX,
+    base_formats.JSON,
+  )
+  
+  list_filter = (
+    ('available_on', DateRangeFilter),
+  )
+
+class PromotionCodeAdmin:
+  def link(self, obj: PromotionCode) -> str:
+    if product := obj.coupon.products.first():
+      html = format_html(
+        '<a href="{url}?{params}">{link_symbol}</a>',
+        url=reverse('shopping_cart', host='www'),
+        params=f'price={product.prices.first().id}&promo={obj.id}',
+        link_symbol=mark_safe('&#128279'),
+      )
+    else:
+      html = 'N/A'
+    return html
